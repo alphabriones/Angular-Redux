46465fc08895e0505cf5026ed3ee7b2ca49ab9a8
alpsbriones <alpha@metricgaming.com>
2023-09-10 17:02:48 +1000

    Added Flex layout, routing and product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { NavigationComponent } from './navigation/navigation.component';
 
@@ -12,22 +13,35 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
 
+import { ProductsComponent } from './components/products/products.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { RouterModule } from '@angular/router';
+
+
+const routes =  [
+  { path: 'products', component: ProductsComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    ProductItemComponent,
+    ProductsComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
+    FlexLayoutModule,
     MatSidenavModule,
     MatIconModule,
     MatButtonModule,
     MatToolbarModule,
     BrowserAnimationsModule,
-    MatListModule
-  
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
